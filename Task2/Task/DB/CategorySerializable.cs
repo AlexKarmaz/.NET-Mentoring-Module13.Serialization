4243565a84f0a0fa235d175bffcf9bad8970a1af
AlexKarmaz <karmazct@mail.ru>
2018-06-10 22:29:01 +0300

    add task2

---
+++ Task2/Task/DB/CategorySerializable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Task.DB
+{
+    [Serializable]
+    public partial class Category
+    {
+        [OnSerializing]
+        public void OnSerializing(StreamingContext context)
+        {
+            SerializationContext serializationContext = context.Context as SerializationContext;
+
+            if (serializationContext != null && serializationContext.TypeToSerialize == typeof(Category))
+            {
+                serializationContext.ObjectContext.LoadProperty(this, c => c.Products);
+            }
+        }
+    }
+}
